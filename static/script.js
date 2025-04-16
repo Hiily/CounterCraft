ca70f97b55a3b1737429fe011d879e5b35ce05b2
Hiily <hiily1378@gmail.com>
2025-04-16 14:44:01 +0200

    change app url

--- static/script.js
+++ static/script.js
@@ -19,7 +19,7 @@ const addCounterForm = document.getElementById('addCounterForm');
 
 let selectedChampion = null;
 let fakeCounters = {}; 
-const apiUrl = "http://localhost:8000"; // a changer
+const apiUrl = "https://ton-app.onrender.com";
 
 
 async function loadChampions() {
